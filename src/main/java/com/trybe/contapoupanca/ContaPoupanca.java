8971431643b3bae584ceb49f59590246597f54eb
Matheus <ymatheus.h@gmail.com>
2023-07-19 15:26:52 -0300

    feat: implements at class ContaPoupanca.java attributes saldo and titular conta and methods depositar, sacar, mostrarSaldo, mostrarTitularConta

--- src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
+++ src/main/java/com/trybe/contapoupanca/ContaPoupanca.java
@@ -1,5 +1,32 @@
 package com.trybe.contapoupanca;
 
+/**
+ * Class ContaPoupança.
+ */
 public class ContaPoupanca {
+
+  private double saldo;
+  private final String titularConta;
+
+  ContaPoupanca(String titularConta, double saldoInicial) {
+    this.titularConta = titularConta;
+    this.saldo = saldoInicial;
+  }
+
+  public void depositar(double valor) {
+    this.saldo += valor;
+  }
+
+  public void sacar(double valor) {
+    this.saldo -= valor;
+  }
+
+  public double mostrarSaldo() {
+    return this.saldo;
+  }
+
+  public String mostrarTitularConta() {
+    return this.titularConta;
+  }
 
 }
